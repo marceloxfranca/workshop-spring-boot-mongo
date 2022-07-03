dab24f787171a10c988423fa4a6a6fb96e535c60
marcelo franca <marceloxfranca@gmail.com>
2022-07-03 18:11:39 -0300

    adicionando class Instantiation

--- src/main/java/com/marcelofranca/workshopmongo/domain/User.java
+++ src/main/java/com/marcelofranca/workshopmongo/domain/User.java
@@ -1,5 +1,8 @@
 package com.marcelofranca.workshopmongo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -7,6 +10,9 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Document(collection = "user")
+@Getter
+@Setter
+@AllArgsConstructor
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -17,37 +23,6 @@ public class User implements Serializable {
 
     public User() {
     }
-
-    public User(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -55,7 +30,6 @@ public class User implements Serializable {
         User user = (User) o;
         return Objects.equals(id, user.id);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(id);
